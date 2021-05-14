5758fecbd5c4afd41c1304b3120c75b6eff5aa7f
Ubeyt Demir <190541075@firat.edu.tr>
2021-05-14 17:04:41 +0300

    Updates

---
+++ precache-manifest.afa8b5bbe253f7d421d3994cefcafc44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "050e9c4b6b8779d9aabd",
+    "url": "/strive-blog-template/static/css/main.5de930ad.chunk.css"
+  },
+  {
+    "revision": "050e9c4b6b8779d9aabd",
+    "url": "/strive-blog-template/static/js/main.18d3a901.chunk.js"
+  },
+  {
+    "revision": "63f3d705cb579291974e",
+    "url": "/strive-blog-template/static/js/runtime~main.bd187177.js"
+  },
+  {
+    "revision": "7f85cfe3f2a5f48f0cdb",
+    "url": "/strive-blog-template/static/css/2.34080a84.chunk.css"
+  },
+  {
+    "revision": "7f85cfe3f2a5f48f0cdb",
+    "url": "/strive-blog-template/static/js/2.57a12f2c.chunk.js"
+  },
+  {
+    "revision": "fdcf400fb0f293ebad2f",
+    "url": "/strive-blog-template/static/js/3.e575d563.chunk.js"
+  },
+  {
+    "revision": "b1a4fbd674b5430864b7a6767a851342",
+    "url": "/strive-blog-template/index.html"
+  }
+];
